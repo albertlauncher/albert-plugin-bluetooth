d10c89e0e95d3c5710a7a933993610a13ba43b61
Manuel Schneider <manuelschneid3r@gmail.com>
2026-01-18 19:36:06 +0100

    chore: Use new icon API

--- src/items.cpp
+++ src/items.cpp
@@ -4,7 +4,7 @@
 #include "bluetoothdevice.h"
 #include "items.h"
 #include <QEventLoop>
-#include <albert/iconutil.h>
+#include <albert/icon.h>
 #include <albert/logging.h>
 #include <albert/messagebox.h>
 #include <albert/networkutil.h>
@@ -49,10 +49,10 @@ QString BluetoothControllerItem::subtext() const
 unique_ptr<albert::Icon> BluetoothControllerItem::icon() const
 {
     switch (controller->state()) {
-    case PoweredOff:  return makeImageIcon(u":bt-inactive"_s);
-    case PoweringOn:  return makeImageIcon(u":bt-change"_s);
-    case PoweredOn:   return makeImageIcon(u":bt-active"_s);
-    case PoweringOff: return makeImageIcon(u":bt-change"_s);
+    case PoweredOff:  return Icon::image(u":bt-inactive"_s);
+    case PoweringOn:  return Icon::image(u":bt-change"_s);
+    case PoweredOn:   return Icon::image(u":bt-active"_s);
+    case PoweringOff: return Icon::image(u":bt-change"_s);
     }
     return {};
 }
@@ -95,10 +95,10 @@ optional<QString> BluetoothControllerItem::toggle() const
 inline static unique_ptr<Icon> connectionStateIcon(BluetoothDevice::State state)
 {
     switch (state) {
-    case Disconnected:  return makeImageIcon(u":bt-inactive"_s);
-    case Connecting:    return makeImageIcon(u":bt-change"_s);
-    case Connected:     return makeImageIcon(u":bt-active"_s);
-    case Disconnecting: return makeImageIcon(u":bt-change"_s);
+    case Disconnected:  return Icon::image(u":bt-inactive"_s);
+    case Connecting:    return Icon::image(u":bt-change"_s);
+    case Connected:     return Icon::image(u":bt-active"_s);
+    case Disconnecting: return Icon::image(u":bt-change"_s);
     }
     return {};
 }
@@ -111,69 +111,69 @@ static unique_ptr<albert::Icon> deviceClassIcon(uint32_t class_of_device)
     case 0x01: // Computer (desktop,notebook, PDA, organizers, .... )
         switch (((class_of_device) & 0b011111100) >> 2) {
         case 0x03:  // Laptop.
-            return makeGraphemeIcon(u"💻"_s);
+            return Icon::grapheme(u"💻"_s);
         case 0x04:  // Handheld PC/PDA (clam shell).
         case 0x05:  // Palm-size PC/PDA.
-            return makeGraphemeIcon(u"📱"_s);
+            return Icon::grapheme(u"📱"_s);
         case 0x06:  // Wearable computer (watch size).
-            return makeGraphemeIcon(u"⌚"_s);
+            return Icon::grapheme(u"⌚"_s);
         case 0x01:  // Desktop workstation.
         case 0x02:  // Server-class computer.
         default:
-            return makeGraphemeIcon(u"🖥️"_s);
+            return Icon::grapheme(u"🖥️"_s);
         }
 
     case 0x02: // Phone (cellular, cordless, payphone, modem, ...)
         switch (((class_of_device) & 0b11111100) >> 2) {
         case 0x01:  // Cellular.
         case 0x03:  // Smart phone.
-            return makeGraphemeIcon(u"📱"_s);
+            return Icon::grapheme(u"📱"_s);
         case 0x02:  // Cordless.
         case 0x04:  // Wired modem or voice gateway.
         case 0x05:  // Common ISDN access.
         default:
-            return makeGraphemeIcon(u"📞"_s);
+            return Icon::grapheme(u"📞"_s);
         }
 
     case 0x03:  // LAN/Network Access point
-        return makeGraphemeIcon(u"🛜"_s);
+        return Icon::grapheme(u"🛜"_s);
 
     case 0x04:  // Audio/Video (headset,speaker,stereo, video display, vcr…
         switch (((class_of_device) & 0b11111100) >> 2) {
         case 0b00001:  // Wearable Headset Device.
-            return makeGraphemeIcon(u"🎧"_s);
+            return Icon::grapheme(u"🎧"_s);
         case 0b00010: // Hands-free Device.
-            return makeGraphemeIcon(u"🗣️"_s);
+            return Icon::grapheme(u"🗣️"_s);
         //case 0b00011:  // (Reserved)
         case 0b00100:  // Microphone
-            return makeGraphemeIcon(u"🎤"_s);
+            return Icon::grapheme(u"🎤"_s);
         case 0b00101:  // Loudspeaker
-            return makeGraphemeIcon(u"🔊"_s);
+            return Icon::grapheme(u"🔊"_s);
         case 0b00110:  // Headphones
-            return makeGraphemeIcon(u"🎧"_s);
+            return Icon::grapheme(u"🎧"_s);
         case 0b00111:  // Portable Audio
-            return makeGraphemeIcon(u"🔊"_s);
+            return Icon::grapheme(u"🔊"_s);
         case 0b01000:  // Car audio
-            return makeComposedIcon(makeGraphemeIcon(u"🚗"_s), makeGraphemeIcon(u"🔊"_s));
+            return Icon::composed(Icon::grapheme(u"🚗"_s), Icon::grapheme(u"🔊"_s));
         case 0b01001:  // Set-top box
-            return makeGraphemeIcon(u"📺"_s);
+            return Icon::grapheme(u"📺"_s);
         case 0b01010:  // HiFi Audio Device
-            return makeGraphemeIcon(u"🎵"_s);
+            return Icon::grapheme(u"🎵"_s);
         case 0b01011:  // VCR
-            return makeGraphemeIcon(u"📼"_s);
+            return Icon::grapheme(u"📼"_s);
         case 0b01100:  // Video Camera
-            return makeGraphemeIcon(u"📹"_s);
+            return Icon::grapheme(u"📹"_s);
         case 0b01101:  // Camcorder
-            return makeGraphemeIcon(u"📷"_s);
+            return Icon::grapheme(u"📷"_s);
         case 0b01110:  // Video Monitor
         case 0b01111:  // Video Display and Loudspeaker
         case 0b10000:  // Video Conferencing
-            return makeGraphemeIcon(u"📺"_s);
+            return Icon::grapheme(u"📺"_s);
         // case 0b10001:  // (Reserved)
         case 0b10010:  // Gaming/Toy
-            return makeGraphemeIcon(u"🧸"_s);
+            return Icon::grapheme(u"🧸"_s);
         default:
-            return makeGraphemeIcon(u"🎵"_s);
+            return Icon::grapheme(u"🎵"_s);
         }
 
     case 0x05:  // Peripheral (mouse, joystick, keyboards, ...)
@@ -181,58 +181,58 @@ static unique_ptr<albert::Icon> deviceClassIcon(uint32_t class_of_device)
         case 0x00:  // "Not a keyboard or pointing device."
             switch ((class_of_device & 0x00111100) >> 2) {
             case 0x01: // Joystick.
-                return makeGraphemeIcon(u"🕹️"_s);
+                return Icon::grapheme(u"🕹️"_s);
             case 0x02: // Gamepad.
-                return makeGraphemeIcon(u"🎮"_s);
+                return Icon::grapheme(u"🎮"_s);
             default:
-                return makeGraphemeIcon(u"❔"_s);
+                return Icon::grapheme(u"❔"_s);
             }
         case 0x01:  // Keyboard.
-            return makeGraphemeIcon(u"⌨️"_s);
+            return Icon::grapheme(u"⌨️"_s);
         case 0x02:  // Pointing device.
             switch ((class_of_device & 0b00111100) >> 2) {
             case 0x05: // Digitizer tablet.
-                return makeGraphemeIcon(u"✍️"_s);
+                return Icon::grapheme(u"✍️"_s);
             default: // Mouse.
-                return makeGraphemeIcon(u"🖱️"_s);
+                return Icon::grapheme(u"🖱️"_s);
             }
         case 0x03:  // Combo device.
-            return makeComposedIcon(makeGraphemeIcon(u"⌨️"_s), makeGraphemeIcon(u"🖱️"_s));
+            return Icon::composed(Icon::grapheme(u"⌨️"_s), Icon::grapheme(u"🖱️"_s));
         default:
-            return makeGraphemeIcon(u"❔"_s);
+            return Icon::grapheme(u"❔"_s);
         }
 
     case 0x06:  // Imaging (printer, scanner, camera, display, ...)
-        return makeGraphemeIcon(u"🖼️"_s);
+        return Icon::grapheme(u"🖼️"_s);
 
     case 0x07:  // Wearable (watch, pager, ...)
         switch (((class_of_device) & 0b11111100) >> 2) {
         case 2:  // Pager.
-            return makeGraphemeIcon(u"📟"_s);
+            return Icon::grapheme(u"📟"_s);
         case 3:  // Jacket.
-            return makeGraphemeIcon(u"🧥"_s);
+            return Icon::grapheme(u"🧥"_s);
         case 4:  // Helmet.
-            return makeGraphemeIcon(u"🪖"_s);
+            return Icon::grapheme(u"🪖"_s);
         case 5:  // Glasses.
-            return makeGraphemeIcon(u"🕶️"_s);
+            return Icon::grapheme(u"🕶️"_s);
         case 1:  // Wrist watch.
         default:
-            return makeGraphemeIcon(u"⌚"_s);
+            return Icon::grapheme(u"⌚"_s);
         }
 
     case 0x08:  // Toy (robot, vehicle, ...)
         switch (((class_of_device) & 0b11111100) >> 2) {
         case 0x01:  // Robot
-            return makeGraphemeIcon(u"🤖"_s);
+            return Icon::grapheme(u"🤖"_s);
         case 0x02:  // Vehicle
-            return makeGraphemeIcon(u"🚗"_s);
+            return Icon::grapheme(u"🚗"_s);
         case 0x03:  // Doll / Action figure
-            return makeGraphemeIcon(u"🧸"_s);
+            return Icon::grapheme(u"🧸"_s);
         case 0x04:  // Controller
-            return makeGraphemeIcon(u"🎮"_s);
+            return Icon::grapheme(u"🎮"_s);
         case 0x05:  // Game
         default:
-            return makeGraphemeIcon(u"🪀"_s);
+            return Icon::grapheme(u"🪀"_s);
         }
 
     case 0x09:
@@ -240,18 +240,18 @@ static unique_ptr<albert::Icon> deviceClassIcon(uint32_t class_of_device)
         switch (((class_of_device) & 0b11111100) >> 2) {
         // case 1:  // Blood Pressure Monitor
         case 2:  // Thermometer
-            return makeGraphemeIcon(u"🌡️"_s);
+            return Icon::grapheme(u"🌡️"_s);
         // case 3:  // Weighing Scale
         // case 4:  // Glucose Meter
         // case 5:  // Pulse Oximeter
         // case 6:  // Heart/Pulse Rate Monitor
         // case 7:  // Health Data Display
         default:
-            return makeGraphemeIcon(u"❤️"_s);
+            return Icon::grapheme(u"❤️"_s);
         }
 
     default:
-        return makeGraphemeIcon(u"❔"_s);
+        return Icon::grapheme(u"❔"_s);
     }
 }
 
@@ -265,7 +265,7 @@ BluetoothDeviceItem::BluetoothDeviceItem(shared_ptr<BluetoothDevice> dev):
 QString BluetoothDeviceItem::id() const { return device->name(); }
 
 unique_ptr<albert::Icon> BluetoothDeviceItem::icon() const
-{ return makeComposedIcon(connectionStateIcon(device->state()), deviceClassIcon(device->classOfDevice())); }
+{ return Icon::composed(connectionStateIcon(device->state()), deviceClassIcon(device->classOfDevice())); }
 
 QString BluetoothDeviceItem::text() const { return device->name(); }
 
