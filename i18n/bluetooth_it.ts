c9e037e0a0bc4e2a01486b105d0de26d19d5982c
Manuel Schneider <manuelschneid3r@gmail.com>
2026-08-08 16:39:41 +0200

    feat: Add italian  translations

---
+++ i18n/bluetooth_it.ts
@@ -0,0 +1,80 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>BluetoothController</name>
+    <message>
+        <source>Powered off</source>
+        <translation>Spento</translation>
+    </message>
+    <message>
+        <source>Powering on</source>
+        <translation>Accensione in corso</translation>
+    </message>
+    <message>
+        <source>Powered on</source>
+        <translation>Acceso</translation>
+    </message>
+    <message>
+        <source>Powering off</source>
+        <translation>Spegnimento in corso</translation>
+    </message>
+</context>
+<context>
+    <name>BluetoothControllerItem</name>
+    <message>
+        <source>Toggle</source>
+        <translation>Attiva/Disattiva</translation>
+    </message>
+    <message>
+        <source>Open Bluetooth settings</source>
+        <translation>Apri impostazioni Bluetooth</translation>
+    </message>
+    <message>
+        <source>Bluetooth</source>
+        <translation>Bluetooth</translation>
+    </message>
+</context>
+<context>
+    <name>BluetoothDevice</name>
+    <message>
+        <source>Connected</source>
+        <translation>Connesso</translation>
+    </message>
+    <message>
+        <source>Connecting</source>
+        <translation>Connessione in corso</translation>
+    </message>
+    <message>
+        <source>Disconnected</source>
+        <translation>Disconnesso</translation>
+    </message>
+    <message>
+        <source>Disconnecting</source>
+        <translation>Disconnessione in corso</translation>
+    </message>
+    <message>
+        <source>Connecting device failed</source>
+        <translation>Connessione del dispositivo non riuscita</translation>
+    </message>
+    <message>
+        <source>Disconnecting device failed</source>
+        <translation>Disconnessione del dispositivo non riuscita</translation>
+    </message>
+</context>
+<context>
+    <name>BluetoothDeviceItem</name>
+    <message>
+        <source>Connect</source>
+        <translation>Connetti</translation>
+    </message>
+    <message>
+        <source>Disconnect</source>
+        <translation>Disconnetti</translation>
+    </message>
+    <message>
+        <source>Failed to power on controller.</source>
+        <translation>Impossibile accedere il controller.</translation>
+    </message>
+</context>
+</TS>
